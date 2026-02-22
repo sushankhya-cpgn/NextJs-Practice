f4fcb7aa51aaa52beec51b18666f3be998b4ee46
sushankhya-cpgn <chapagain.sushankhya@gmail.com>
2026-02-22 14:22:59 +0545

    Basic routing and understanding file structure, font and image optimization

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({
+    subsets: ['latin'],
+})
+
+export const lustinana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700']
+});
